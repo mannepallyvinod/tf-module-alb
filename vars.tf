9380548162232885443d1dd430f599a38dc45b6c
mannepallyvinod <111588987+mannepallyvinod@users.noreply.github.com>
2023-04-25 11:51:57 +0530

    Add files via upload

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "name" {}
+variable "env" {}
+variable "tags" {}
+variable "internal" {}
+variable "load_balancer_type" {}
+variable "enable_deletion_protection" {
+  default = false
+}
+variable "subnets" {}
+variable "vpc_id" {}
+variable "allow_cidr" {}
